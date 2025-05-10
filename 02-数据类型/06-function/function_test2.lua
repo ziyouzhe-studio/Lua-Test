eeffa2c53a1310538a7a5215891fb5c575d381b8
ziyouzhe-studio <wuwei20015291@outlook.com>
2025-05-10 22:50:11 +0800

    更新了一些内容

---
+++ 02-数据类型/06-function/function_test2.lua
@@ -0,0 +1,16 @@
+-- function_test2.lua
+-- function 可以以匿名函数(anonymous function)的方式通过参数传递
+-- function_test2.lua 脚本文件
+function testFun(tab,fun)
+        for k ,v in pairs(tab) do
+                print(fun(k,v));
+        end
+end
+
+
+tab={key1="val1",key2="val2"};
+testFun(tab,
+function(key,val)--匿名函数
+        return key.."="..val;
+end
+);
